745b23e8c53edcafd60df8356571e2c3fa610cfb
jinguihua <156711203@qq.com>
2024-06-20 09:57:29 +0800

    changed

--- base.go
+++ base.go
@@ -5,12 +5,14 @@ import (
 	"gorm.io/gorm/clause"
 )
 
+// 共享锁，其它事务 只能读取记录，不能修改记录
 func ShareLock(tx *gorm.DB) *gorm.DB {
 	return tx.Clauses(clause.Locking{
 		Strength: "share",
 	})
 }
 
+// 独占锁：其它事务不能读取记录
 func ExclusiveLock(tx *gorm.DB) *gorm.DB {
 	return tx.Clauses(clause.Locking{
 		Strength: "update",
